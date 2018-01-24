4b26a2f6d36d53a9ccff3c019aceaa5a02b51935
shoanchikato <sjchikato@gmail.com>
2018-01-24 20:12:24 +0200

    change server.js line 19 port 4200 to 3000 for CORS

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 // Allow requests from Angular
 app.use((req, res, next) => {
   // Website you wish to allow to connect
-   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
    // Request methods you wish to allow
    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
    // Request headers you wish to allow
